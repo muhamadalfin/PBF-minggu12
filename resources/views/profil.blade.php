f637580a93939b508b01eb4b914c9e2f7c4a39ce
muhamadalfin <malfin416@gmail.com>
2021-05-05 21:54:24 +0700

    Minggu 12

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/css/profil.css">
+<title>BIODATA DIRI</title>
+<body>
+<br><br><br>
+<center>
+<div class="card border-info mb-3" style="max-width: 40rem;">
+  <div class="card-header bg-transparent border-success">BIODATA DIRI</div>
+  <div class="card-body text-info">
+    <table class="table table-hover" style="width:500px">
+        <tbody>
+            <tr>
+                <th scope="row">#</th>
+                <td>Nama</td>
+                <td>Muhamad Alfin Rizqullah N.</td>
+            </tr>
+            <tr>
+                <th scope="row">#</th>
+                <td>Jurusan</td>
+                <td>Teknologi Informasi</td>
+            </tr>
+            <tr>
+                <th scope="row">#</th>
+                <td>Program Studi</td>
+                <td>D4 Teknik Informatika</td>
+            </tr>
+            <tr>
+                <th scope="row">#</th>
+                <td>Jenis Kelamin</td>
+                <td>Laki - Laki</td>
+            </tr>
+            <tr>
+                <th scope="row">#</th>
+                <td>Alamat</td>
+                <td>Malang</td>
+            </tr>
+            <tr>
+                <th scope="row">#</th>
+                <td>Tempat / Tgl Lahir</td>
+                <td>Malang, 15 Maret 2000</td>
+            </tr>
+            <tr>
+                <th scope="row">#</th>
+                <td>Agama</td>
+                <td>Islam</td>
+            </tr>
+            <tr>
+                <th scope="row">#</th>
+                <td>Status Mahasiswa</td>
+                <td>Aktif</td>
+            </tr>
+        </tbody>
+    </table>
+  </div>
+  <div class="card-footer bg-transparent border-success">PEMROGRAMAN BERBASIS FRAMEWORK</div>
+</div>
+</center>
+    
+</body>
+</html>
